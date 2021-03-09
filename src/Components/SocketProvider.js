37c7a0fb8af8ded9c6f9d78c0b26fa7254ebe5ef
Narasimha <naninarasimha@gmail.com>
2021-03-09 22:17:44 +0530

    messages deliver while offline and chat with anyone on online1.9

--- src/Components/SocketProvider.js
+++ src/Components/SocketProvider.js
@@ -12,7 +12,7 @@ function SocketProvider({children,id}) {
 
     useEffect(()=>{
         const newSocket=io(
-            'http://127.0.0.1:5000/',
+            'https://gravel-special-leech.glitch.me',
             {query:{id}}
         )
         setSocket(newSocket)
